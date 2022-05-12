7ee71b464bb77a06977bc482f3d896404f198175
Gus Varallo <gustavo@varallo.me>
2022-05-12 17:24:09 -0300

    feat: add remove button component

---
+++ src/components/RemoveButton.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { message, Button } from 'antd'
+import { PARTICIPANTS } from '../helpers/constants'
+import { deleteMessages, deleteParticipants } from '../services/functions'
+
+class RemoveButton extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      target: props.target,
+      buttonText: '',
+      alertMessage: '',
+      loading: false
+    }
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      buttonText:
+        this.state.target === PARTICIPANTS
+          ? '- Remove Participants'
+          : '- Remove Messages',
+      alertMessage:
+        this.state.target === PARTICIPANTS
+          ? 'Participants removed!'
+          : 'Messages Removed'
+    })
+  }
+
+  handleRemoveButton = async () => {
+    try {
+      this.setState({ loading: true })
+
+      const response =
+        this.state.target === PARTICIPANTS
+          ? deleteParticipants()
+          : deleteMessages()
+
+      this.setState({ loading: false })
+      this.setState({ response: response })
+      message.success(this.state.alertMessage)
+    } catch (error) {
+      console.log(error)
+      this.setState({ loading: false })
+      message.error('An error has occured')
+    }
+  }
+
+  render() {
+    const { loading, buttonText } = this.state
+    return (
+      <Button
+        loading={loading}
+        onClick={() => this.handleRemoveButton()}
+        style={{ width: '100%', marginTop: '10px' }}
+      >
+        {buttonText}
+      </Button>
+    )
+  }
+}
+
+export default RemoveButton
