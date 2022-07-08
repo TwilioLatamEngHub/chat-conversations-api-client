9f72eab86d4569f5b6d80abca7e46669689fbacc
Gus Varallo <gustavo@varallo.me>
2022-07-08 11:40:44 -0300

    refactor: change HeaderItem to ts

--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-
 interface HeaderItemProps {
   children: JSX.Element | JSX.Element[]
   style?: any
 }
 
-export const HeaderItem = ({ children, style }: HeaderItemProps) => {
+export const HeaderItem = ({
+  children,
+  style
+}: HeaderItemProps): JSX.Element => {
   const finalStyle = { padding: '0 19px 0 19px', ...style }
 
   return <div style={finalStyle}>{children}</div>
