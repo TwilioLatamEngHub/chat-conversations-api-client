cf308b254e311a760d2879f67e0ff04878f9c4c7
Gus Varallo <gustavo@varallo.me>
2022-07-12 20:11:43 -0300

    refactor: wip make modal only on create convo

--- src/components/ConversationsModal.tsx
+++ src/components/ConversationsModal.tsx
@@ -1,45 +1,64 @@
-import { useCallback, useContext, useState } from 'react'
+import { useCallback, useContext } from 'react'
 import { Button, Form, Input, Modal } from 'antd'
 import { UserOutlined, MessageOutlined } from '@ant-design/icons'
-import { Client, Conversation as ConversationType } from '@twilio/conversations'
 
 import { ConversationsContext } from '../contexts'
-import { getToken } from '../services/functions'
+import { addParticipant, createConversation } from '../services/functions'
 import { Conversation } from './Conversation'
 
 export const ConversationsModal = (): JSX.Element => {
   const {
     showModal,
     setShowModal,
-    isNewConversation,
-    selectedConversationSid,
     setConversationContent,
-    setIdentity
+    identity,
+    setIdentity,
+    isLoading,
+    setIsLoading
   } = useContext(ConversationsContext)
-  const [isLoading, setIsLoading] = useState(false)
 
-  const initConversation = useCallback(
-    async (identity: string, friendlyName: string) => {
-      const { accessToken } = await getToken(identity)
+  const initConversation = useCallback(async (friendlyName: string) => {
+    try {
+      const { conversation } = await createConversation(friendlyName)
+      console.log('New conversation created:')
+      console.log(conversation)
 
-      if (accessToken) {
-        const client = new Client(accessToken)
-        let conversation: ConversationType
+      const { participantSid } = await addParticipant({
+        participantType: 'chat',
+        conversationSid: conversation.sid,
+        identity
+      })
+      console.log('Participant added. SID or exists:')
+      console.log(participantSid)
 
-        if (isNewConversation && friendlyName) {
-          conversation = await client.createConversation({
-            friendlyName: friendlyName
-          })
-        } else {
-          conversation = await client.getConversationBySid(
-            selectedConversationSid
-          )
-        }
-        setConversationContent(<Conversation conversation={conversation} />)
-      }
-    },
-    [isNewConversation, setConversationContent, selectedConversationSid]
-  )
+      setConversationContent(<Conversation conversation={conversation} />)
+    } catch (error) {
+      console.log(error)
+    }
+
+    // const { accessToken } = await getToken('gus')
+
+    // if (accessToken) {
+    //   const client = new Client(accessToken)
+    //   let conversation: ConversationType
+
+    //   if (isNewConversation && friendlyName) {
+    //     conversation = await client.createConversation({
+    //       friendlyName: friendlyName
+    //     })
+    //   } else {
+    //     conversation = await client.getConversationBySid(
+    //       selectedConversationSid
+    //     )
+    //   }
+    //   // Fix conversation add
+    //   conversation.add(identity)
+    //   setConversationContent(<Conversation conversation={conversation} />)
+    // }
+
+    // Need to run only once, so empty deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   const handleCancel = () => {
     setShowModal(false)
@@ -51,7 +70,7 @@ export const ConversationsModal = (): JSX.Element => {
     try {
       setIsLoading(true)
       setIdentity(identity)
-      await initConversation(identity, friendlyName)
+      await initConversation(friendlyName)
       setShowModal(false)
       setIsLoading(false)
     } catch (error) {
@@ -69,18 +88,16 @@ export const ConversationsModal = (): JSX.Element => {
       footer={null}
     >
       <Form layout='vertical' onFinish={onFinish}>
-        {isNewConversation && (
-          <Form.Item
-            label='Create a conversastion name:'
-            name='friendlyName'
-            rules={[{ required: true }]}
-          >
-            <Input
-              prefix={<MessageOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
-              placeholder='Conversation name'
-            />
-          </Form.Item>
-        )}
+        <Form.Item
+          label='Create a conversastion name:'
+          name='friendlyName'
+          rules={[{ required: true }]}
+        >
+          <Input
+            prefix={<MessageOutlined style={{ color: 'rgba(0,0,0,.25)' }} />}
+            placeholder='Conversation name'
+          />
+        </Form.Item>
         <Form.Item
           label='Please input your identity below:'
           name='identity'
